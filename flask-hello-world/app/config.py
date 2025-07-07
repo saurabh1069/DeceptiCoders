e06b70a7a3bdcb49c43f2d90596a7af7c86ae42f
AmrutaPendseBatra <140156054+AmrutaPendseBatra@users.noreply.github.com>
2025-07-07 09:29:26 +0530

    cred removed
    cred removed

--- flask-hello-world/app/config.py
+++ flask-hello-world/app/config.py
@@ -1,3 +1,3 @@
 import os
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://amrupendse:PaFZ6gfFMBSyX7Qj@cluster-monitoring.udx2ebr.mongodb.net/Log_Monitoring?retryWrites=true&w=majority")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/Log_Monitoring")
